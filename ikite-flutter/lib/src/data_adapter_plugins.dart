8e775537f514745f1e98fdd12e13fa3fc6922433
Liplum <Li_plum@outlook.com>
2022-12-16 03:12:57 +0800

    Added DateTimeDataAdapter in DefaultFlutterDataAdapterPlugin.

--- ikite-flutter/lib/src/data_adapter_plugins.dart
+++ ikite-flutter/lib/src/data_adapter_plugins.dart
@@ -3,15 +3,17 @@ part of '../ikite_flutter.dart';
 // ignore: non_constant_identifier_names
 void DefaultFlutterDataAdapterPlugin(IKite ikite) {
   ikite._r(ColorDataAdapter());
+  ikite._r(DateTimeDataAdapter());
 }
 
-extension _DataAdapterEx on IKite{
-  void _r<T>(DataAdapter<T> adapter){
-    if(!hasAdapterOf(adapter.typeName)){
+extension _DataAdapterEx on IKite {
+  void _r<T>(DataAdapter<T> adapter) {
+    if (!hasAdapterOf(adapter.typeName)) {
       registerAdapter(adapter);
     }
   }
 }
+
 class ColorDataAdapter extends DataAdapter<Color> {
   @override
   String get typeName => "dart.ui.Color";
@@ -25,8 +27,25 @@ class ColorDataAdapter extends DataAdapter<Color> {
 
   @override
   Map<String, dynamic> toJson(ParseContext ctx, Color obj) {
+    return {"value": obj.value};
+  }
+}
+
+class DateTimeDataAdapter extends DataAdapter<DateTime> {
+  @override
+  String get typeName => "dart.core.DateTime";
+
+  @override
+  DateTime fromJson(RestoreContext ctx, Map<String, dynamic> json) {
+    return DateTime.fromMillisecondsSinceEpoch(
+      json["milliseconds"],
+    );
+  }
+
+  @override
+  Map<String, dynamic> toJson(ParseContext ctx, DateTime obj) {
     return {
-      "value": obj.value
+      "milliseconds": obj.millisecondsSinceEpoch,
     };
   }
 }
