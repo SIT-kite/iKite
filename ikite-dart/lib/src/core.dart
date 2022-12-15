6aa6549d39200767c11f8580f891493565be7715
Liplum <Li_plum@outlook.com>
2022-12-15 17:51:29 +0800

    Added a simple plugin system.

--- ikite-dart/lib/src/core.dart
+++ ikite-dart/lib/src/core.dart
@@ -5,3 +5,17 @@ abstract class IKite with IKiteConverter {}
 class _IKiteImpl extends IKite {
   _IKiteImpl();
 }
+
+typedef IKitePlugin = void Function(IKite kite);
+
+extension IKitePluginEx on IKite {
+  void addPlugin(IKitePlugin plugin) {
+    plugin(this);
+  }
+
+  void addPlugins(List<IKitePlugin> plugins) {
+    for (final plugin in plugins) {
+      addPlugin(plugin);
+    }
+  }
+}
